513226eba30851570a6014b81085e42d39b3b9b1
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-09-12 18:29:49 +0330

    Basic Form project done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const[valid,setValid] =useState(false);
+  const[submit,setSubmit]=useState(false)
+  const[inputValue,setInputValue] =useState({
+    firstName: '',
+    LastName: '',
+    email: ''
+  })
+  const firstNameChange =(event)=>{
+    setInputValue({...inputValue,firstName:event.target.value})
+    console.log(inputValue);
+  }
+  const LastNameChange =(event)=>{
+    setInputValue({...inputValue,LastName:event.target.value})
+  }
+  const emailChange =(event)=>{
+    setInputValue({...inputValue,email:event.target.value})
+  }
+  const submitHandler = (event)=>{
+    event.preventDefault()
+    if(inputValue.firstName && inputValue.LastName && inputValue.email){
+      setValid(true)
+    }
+    setSubmit(true)
+  }
+
   return (
-    <div class="form-container">
-      <form class="register-form">
+    <div className="form-container">
+      <form className="register-form" onSubmit={submitHandler}>
+        {submit && valid ?<div className='success-message'> Success ! your form registered.</div> :null}
        
         <input
+          onChange={firstNameChange}
+          value={inputValue.firstName}
           id="first-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
         />
+        {submit && !inputValue.firstName ?<span>please enter a first name</span> : null}
       
         <input
+        onChange={LastNameChange}
+        value={inputValue.LastName}
           id="last-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
         />
+        {submit && !inputValue.LastName ?<span>please enter a last name</span>:null}
       
         <input
+        onChange={emailChange}
+        value={inputValue.email}
           id="email"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Email"
           name="email"
         />
+        {submit && !inputValue.email ?<span>please enter an email</span>:null}
        
-        <button class="form-field" type="submit">
+        <button className="form-field" type="submit">
           Register
         </button>
       </form>
