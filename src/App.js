d0f418ece635021f2370048a5e4d3a7679ff4904
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-09-12 16:33:34 +0330

    App.js added and jsx basic done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div class="form-container">
+      <form class="register-form">
+       
+        <input
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
       
+        <input
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+      
+        <input
+          id="email"
+          class="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+       
+        <button class="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
